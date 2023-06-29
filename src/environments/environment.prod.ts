da5c910c945314f0a1b838df1188566d277f5ec0
BuildTools <1902343@utrivieramaya.edu.mx>
2023-06-29 11:38:10 -0500

    prod environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   SECRET_KEY: '`>V(CMAZ~c;GNb,BA*mj[]mEv[aR]*Roe>n}Lz9)',
-  API_URL: 'http://localhost:5000/',
+  API_URL: 'http://45.55.66.121/',
 };
