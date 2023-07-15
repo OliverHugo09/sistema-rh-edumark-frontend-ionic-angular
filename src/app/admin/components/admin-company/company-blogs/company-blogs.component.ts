52fe427eb8cfa81408299ece895e16569474ffb7
BuildTools <1902343@utrivieramaya.edu.mx>
2023-07-14 23:00:38 -0500

    toolbar blogs configurada

--- src/app/admin/components/admin-company/company-blogs/company-blogs.component.ts
+++ src/app/admin/components/admin-company/company-blogs/company-blogs.component.ts
@@ -52,16 +52,11 @@ export class CompanyBlogsComponent implements OnInit {
     this.modules = {
       toolbar: [
         ['bold', 'italic', 'underline', 'strike'],
-        [{ 'align': [] }],
         [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-        [{ 'font': [] }],
-        ['blockquote', 'code-block'],
         [{ 'header': 1 }, { 'header': 2 }],
         [{ 'script': 'sub' }, { 'script': 'super' }],
-        [{ 'indent': '-1' }, { 'indent': '+1' }],
-        [{ 'direction': 'rtl' }],
         [{ 'size': ['small', false, 'large', 'huge'] }],
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'header': [1, 2, 3, 4, 5, false] }],
         [{ 'color': [] }, { 'background': [] }],
         ['link', 'image', 'video'],
         ['clean']
