e6b4e4212e9c8218145602e611c662d9bd99b22f
BuildTools <1902343@utrivieramaya.edu.mx>
2023-07-17 22:04:10 -0500

    Funcionalidad de blogs lista

--- src/app/admin/components/admin-company/company-blogs/company-blogs.component.ts
+++ src/app/admin/components/admin-company/company-blogs/company-blogs.component.ts
@@ -1,21 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Blog } from '../../../../interface/blog';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AlertController } from '@ionic/angular';
-
-import { Options } from 'ngx-quill-upload';
-
-import Quill from 'quill';
-import ImageUploader from 'quill-image-uploader';
-import { ImageHandler, VideoHandler } from 'ngx-quill-upload';
-
-Quill.register('modules/imageUploader', ImageUploader);
-Quill.register('modules/imageHandler', ImageHandler);
-Quill.register('modules/videoHandler', VideoHandler);
+import { BlogCompanyService } from '../../../../mobile/connection/api/blogs-company.service';
 
 let empresaIdNumerico = localStorage.getItem('empresaId');
 let empresaId = parseInt(empresaIdNumerico);
@@ -26,143 +13,86 @@ let empresaId = parseInt(empresaIdNumerico);
   styleUrls: ['./company-blogs.component.scss'],
 })
 export class CompanyBlogsComponent implements OnInit {
-  blogContent: string;
-  modules: any;
-  editor: Quill;
-  blogForm: FormGroup;
 
-  blog: Blog = {
-    id: null,
-    content: '',
-    entidadId: null,
-    empresaId: 0,
-    title: '',
-    description: ''
-  };
+  blogs: Blog[];
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private sanitizer: DomSanitizer, private alertController: AlertController) { }
-
-  ngAfterViewInit() {
-    this.editor.on('text-change', () => {
-      this.blogContent = this.editor.root.innerHTML;
-    });
-  }
+  constructor(
+    private alertController: AlertController,
+    private blogService: BlogCompanyService
+  ) { }
 
   ngOnInit() {
-    this.modules = {
-      toolbar: [
-        ['bold', 'italic', 'underline', 'strike'],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-        [{ 'header': 1 }, { 'header': 2 }],
-        [{ 'script': 'sub' }, { 'script': 'super' }],
-        [{ 'size': ['small', false, 'large', 'huge'] }],
-        [{ 'header': [1, 2, 3, 4, 5, false] }],
-        [{ 'color': [] }, { 'background': [] }],
-        ['link', 'image', 'video'],
-        ['clean']
-      ],
-      // Configura el módulo imageHandler para subir imágenes
-      imageHandler: {
-        upload: (file: File) => {
-          return new Promise<string>((resolve, reject) => {
-            const formData = new FormData();
-            formData.append('upload', file);
+    this.loadBlogs();
+  }
 
-            // Realiza una solicitud HTTP para cargar la imagen al servidor
-            this.http.post(environment.API_URL + 'upload-image', formData)
-              .subscribe((response: any) => {
-                // Obtiene la URL de la imagen desde la respuesta del servidor
-                const imageUrl = response.imageUrl;
+  async loadBlogs() {
+    // Obtén el empresaId del localStorage y conviértelo a número
+    const empresaIdNumerico = localStorage.getItem('empresaId');
+    const empresaId = parseInt(empresaIdNumerico);
 
-                // Verifica que la URL de la imagen esté definida
-                if (imageUrl) {
-                  // Inserta la imagen en el editor
-                  const range = this.editor.getSelection(true);
-                  this.editor.insertEmbed(range.index, 'image', imageUrl, 'user');
-
-                  // Resuelve la promesa con la URL de la imagen
-                  resolve(imageUrl);
-                } else {
-                  reject('Error al cargar la imagen');
-                }
-              }, (error) => {
-                console.error('Error al cargar la imagen', error);
-                reject('Error al cargar la imagen');
-              });
-          });
-        },
-      } as Options,
-    };
-
-    this.editor = new Quill('#editor', {
-      modules: this.modules,
-    });
-
-    this.editor.on('editor-change', (event: any) => {
-      if (event.source === 'user' && event.delta) {
-        const hasImageDelta = event.delta.ops.some((op: any) => op.insert && op.insert.image);
-        if (!hasImageDelta) {
-          this.blogContent = this.editor.root.innerHTML;
-        }
+    // Llama al servicio para obtener la lista de blogs
+    this.blogService.getBlogByCompany(empresaId).subscribe(
+      (blogs) => {
+        this.blogs = blogs;
+      },
+      (error) => {
+        console.error('Error al obtener la lista de blogs', error);
       }
-    });
-
-    this.blogForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      description: ['', Validators.required]
-    });
+    );
   }
 
-  sanitizeHtml(html: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(html);
-  }
-
-  createBlog(): void {
-    if (this.blogContent === undefined || this.blogContent === null) {
-      this.blogForm.markAllAsTouched();
-      const alert = this.alertController.create({
-        header: 'Error ❌',
-        subHeader: 'Blog invalido',
-        message: 'Por favor llena el blog con el contenido requerido',
-        buttons: ['OK'],
-      }).then((alert) => {
-        alert.present();
-      });
-      return;
-    }
-
-    const blogData = {
-      title: this.blogForm.value.title,
-      description: this.blogForm.value.description,
-      content: this.blogContent,
-      empresaId: empresaId,
-      // Otros datos del blog
-    };
-
-    this.http.post(`${environment.API_URL}blog`, blogData)
-      .subscribe(
-        () => {
-          const alert = this.alertController.create({
-            header: 'Éxito ✔️',
-            subHeader: 'Blog creado',
-            message: 'Blog creado exitosamente',
-            buttons: ['OK'],
-          }).then((alert) => {
-            alert.present();
-          });
-          console.log('Blog creado exitosamente');
+  async deleteBlog(blogId: number) {
+    const alert = await this.alertController.create({
+      header: 'Eliminar blog',
+      message: '¿Estás seguro de que quieres eliminar este blog?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
         },
-        (error) => {
-          const alert = this.alertController.create({
-            header: 'Error ❌',
-            subHeader: 'Error al crear el blog',
-            message: 'No se pudo crear el blog',
-            buttons: ['OK'],
-          }).then((alert) => {
-            alert.present();
-          });
-          console.error('Error al crear el blog', error);
-        }
-      );
+        {
+          text: 'Eliminar',
+          cssClass: 'danger',
+          handler: () => {
+            this.blogService.deleteBlog(blogId).subscribe(
+              () => {
+                // Eliminación exitosa, aquí puedes mostrar una notificación de éxito
+                this.showSuccessAlert('Blog eliminado exitosamente');
+                // Vuelve a cargar la lista de blogs después de eliminar uno
+                this.loadBlogs();
+              },
+              (error) => {
+                console.error('Error al eliminar el blog', error);
+                // Aquí puedes mostrar una notificación de error si lo deseas
+                this.showErrorAlert('Error al eliminar el blog');
+              }
+            );
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async showSuccessAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: message,
+      buttons: ['Aceptar'],
+    });
+
+    await alert.present();
+  }
+
+  async showErrorAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: message,
+      buttons: ['Aceptar'],
+    });
+
+    await alert.present();
   }
 }
