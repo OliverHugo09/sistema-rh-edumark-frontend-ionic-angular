af0aa60c03421b9a09594d94cce15cb1ea485470
BuildTools <1902343@utrivieramaya.edu.mx>
2023-07-27 21:47:23 -0500

    enviroment domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   SECRET_KEY: '`>V(CMAZ~c;GNb,BA*mj[]mEv[aR]*Roe>n}Lz9)',
-  API_URL: 'http://45.55.66.121/',
+  API_URL: 'https://sistemarh-edumark.sytes.net/',
 };
 
 /*
